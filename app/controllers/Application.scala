e87aab7ab82027f74905b1873ea62b466230c1da
Mandubian <pascal.voitot@mandubian.org>
2011-12-21 19:03:22 +0100

    new structures

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,13 +2,63 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.json._
 
 object Application extends Controller {
+    object WineColor extends Enumeration {
+     type WineColor = Value
+     val WHITE, RED, ROSE = Value
+    }
+    
+    import WineColor._
+
   	case class Producer(name:String, place: String)
-  	case class Wine(title:String, year:Int, alcohol:Float, producer:Producer, grapes: Map[String, Float], nose:List[String]) 
+
+  	trait Wine {
+      def title:String
+      def year:Int
+      def alcohol:Float
+      def producer:Producer
+      def grapes: Map[String, Float]
+      def nose:List[String]
+      def color:WineColor
+    }
+
+    case class WhiteWine(
+      override val title:String, 
+      override val year:Int, 
+      override val alcohol:Float, 
+      override val producer:Producer, 
+      override val grapes: Map[String, Float], 
+      override val nose:List[String]) extends Wine {
+      
+      override val color=WHITE
+    }    
+
+    case class RedWine(
+      override val title:String, 
+      override val year:Int, 
+      override val alcohol:Float, 
+      override val producer:Producer, 
+      override val grapes: Map[String, Float], 
+      override val nose:List[String]) extends Wine {
+      
+      override val color=RED
+    }
+
+    case class RoseWine(
+      override val title:String, 
+      override val year:Int, 
+      override val alcohol:Float, 
+      override val producer:Producer, 
+      override val grapes: Map[String, Float], 
+      override val nose:List[String]) extends Wine {
+      
+      override val color=ROSE
+    }
 
   	val bourgogne_castagnier = 
-  		Wine(
+  		WhiteWine(
 	  		"Bourgogne", 2009, 12.5F, 
   			Producer("Castagnier", "Morey Saint Denis"),
   			grapes = Map("Pinot Noir" -> 100),
@@ -16,12 +66,23 @@ object Application extends Controller {
   		)
 
   	val chateauneuf_mercier = 
-  		Wine(
+  		RedWine(
 	  		"Chateau du Pape", 2007, 15.5F, 
   			Producer("Mercier", "Courthézon"),
   			grapes = Map("Grenache" -> 80, "Syrah" -> 20),
   			nose = List("red fruits", "cafe", "woods", "leather", "pepper")
   		)
+
+    val vin_jaune_letoile = 
+      WhiteWine(
+        "Vin Jaune", 1982, 13F, 
+        Producer("L'Etoile", "Arbois"),
+        grapes = Map("savagnin" -> 100),
+        nose = List("nuts", "dried fruits", "stone")
+      )  
+
+
+    val wines = List(bourgogne_castagnier, chateauneuf_mercier, vin_jaune_letoile)
 
   	val jsonStr = """{
   		"wines": [
@@ -51,23 +112,49 @@ object Application extends Controller {
   	}
   	"""	
   
+    implicit object ProducerFormat extends Format[Producer] {
+      def reads(json: JsValue): Producer = Producer(
+        (json \ "name").as[String],
+        (json \ "place").as[String]
+      )
+      def writes(p: Producer): JsValue = JsObject(Map(
+        "name" -> JsNumber(BigDecimal(p.name)),
+        "place" -> JsString(p.place)
+        )
+      )
+      
+    }
+
   	def strToJson = Action {
   		import play.api.json._
-
-  	
 
   		val json = parseJson(jsonStr)
     	//Ok(views.html.index("Your new application is ready."))
    		Ok(json)
   	}
 
+    def structureToJson = Action {
+      import play.api.json._
+
+      val bourgAsMap = Map(
+        "name" -> "Bourgogne",
+        "year" -> 2009, 
+        "alcoohol" -> 12.5F, 
+        "producer" -> Map("name" -> "Castagnier", "place" -> "Morey Saint Denis"),
+        "grapes" -> Map("Pinot Noir" -> 100),
+        "nose" -> List("cherry", "red fruits", "very light vanilla")
+      )
+      //Ok(views.html.index("Your new application is ready."))
+      Ok(toJson(bourgAsMap))
+    }
+
   	def classToJson = Action {
   		import play.api.json._
 
 
-  		//val json = parseJson(jsonStr)
+  		val json = toJson[WhiteWine](bourgogne_castagnier)
     	//Ok(views.html.index("Your new application is ready."))
-    	Ok("")
+    	Ok("test")
   	}
   
 }
